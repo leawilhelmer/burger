3da9489ed2ecf21e6b362b8b3e0b377f084d6eef
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-01-11 22:37:52 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 8080;
+
 const app = express();
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const express = require("express-handlebars");
+app.use(bodyParser.json());
+
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
