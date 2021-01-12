362227aab7390f96b1ffa3809ce8d9d594f09af1
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-01-11 22:31:01 -0800

    added sql

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Hamburger', true);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie Burger' true);
+
+SELECT * FROM burgers;
