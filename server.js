ba9766874fd322811b150b61b7188d8ae04f7eb0
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-01-12 20:59:18 -0800

    updated js files

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mysql = require('mysql');
 
-const PORT = process.env.PORT || 8030;
-
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
-
+var bodyParser = require('body-parser');
 
 var app = express();
-
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 
 var router = require('./controllers/burgers_controller');
 app.use('/', router);
 
+
+var PORT = 8030;
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
